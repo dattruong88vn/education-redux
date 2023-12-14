15c63db03a4db40f92bf0d0dea164bcf57e58d25
dat.truong <dat.truong@secomm.vn>
2023-12-14 13:57:36 +0700

    multiple data in reducer

--- redux-demo/index.js
+++ redux-demo/index.js
@@ -3,6 +3,7 @@ const redux = require("redux");
 const { createStore } = redux;
 
 const BUY_CAKE = "BUY_CAKE";
+const BUY_ICE_CREAM = "BUY_ICE_CREAM";
 
 // action creator
 function buyCake() {
@@ -10,10 +11,16 @@ function buyCake() {
     type: BUY_CAKE,
   };
 }
+function buyIceCream() {
+  return {
+    type: BUY_ICE_CREAM,
+  };
+}
 
 // initial state
 const initialState = {
   numberOfCakes: 10,
+  numberOfIceCreams: 20,
 };
 
 // reducer
@@ -23,6 +30,12 @@ const reducer = (state = initialState, action) => {
       return {
         ...state,
         numberOfCakes: state.numberOfCakes - 1,
+      };
+    }
+    case BUY_ICE_CREAM: {
+      return {
+        ...state,
+        numberOfIceCreams: state.numberOfIceCreams - 1,
       };
     }
 
@@ -40,6 +53,7 @@ console.log("initial state", store.getState());
 
 // dispatch action
 store.dispatch(buyCake());
+store.dispatch(buyIceCream());
 
 console.log("state", store.getState());
 store.dispatch(buyCake());
@@ -52,6 +66,8 @@ const unsubscribe = store.subscribe(() => {
 
 store.dispatch(buyCake());
 store.dispatch(buyCake());
+store.dispatch(buyIceCream());
+store.dispatch(buyIceCream());
 console.log("state", store.getState());
 
 // unregister
