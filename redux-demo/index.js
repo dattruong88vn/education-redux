1b17f3950e85e64946e8b1fd6e215743906cbf1b
dat.truong <dat.truong@secomm.vn>
2023-12-14 11:51:11 +0700

    action, reducer, store, getState, dispatch and register listener

--- redux-demo/index.js
+++ redux-demo/index.js
@@ -1,3 +1,7 @@
+const redux = require("redux");
+
+const { createStore } = redux;
+
 const BUY_CAKE = "BUY_CAKE";
 
 // action creator
@@ -6,3 +10,46 @@ function buyCake() {
     type: BUY_CAKE,
   };
 }
+
+// initial state
+const initialState = {
+  numberOfCakes: 10,
+};
+
+// reducer
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case BUY_CAKE: {
+      return {
+        ...state,
+        numberOfCakes: state.numberOfCakes - 1,
+      };
+    }
+
+    default: {
+      return state;
+    }
+  }
+};
+
+// store
+const store = createStore(reducer);
+
+// get state
+console.log("initial state", store.getState());
+
+// dispatch action
+store.dispatch(buyCake());
+
+console.log("state", store.getState());
+store.dispatch(buyCake());
+console.log("state", store.getState());
+
+const unsubscribe = store.subscribe(() => {
+  console.log("update state", store.getState());
+});
+
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+console.log("state", store.getState());
+unsubscribe();
